adfa2c6a0b3a8ff43bfd50992acc65d947c4494f
1stdomonov <98898208+1stdomonov@users.noreply.github.com>
2022-10-25 18:49:25 -0500

    Initial commits of CSS, Index, and JS files

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//pop-up message prints to index page when it first loads
+alert("Hello World");
+
+//declare variable favoriteFood and assign value of choice
+let favoriteFood = "Pizza!";
+
+//text/item prints to index page
+document.write(favoriteFood);
