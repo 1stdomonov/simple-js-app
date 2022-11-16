0cae412410e77ae8dd230e15a24039b2ed6166c4
1stdomonov <98898208+1stdomonov@users.noreply.github.com>
2022-11-15 18:18:19 -0600

    wrapped pokemonList in IFFE and updated forEach loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,28 @@
-//add several pokemon objects to an array
+//wrap pokemonList array in IIFE and assign to repo variable
+let pokemonRepository = (function () {
 pokemonList = [
     {name: 'Bulbasaur', height: 2.2, type: ['grass', 'poison']},
     {name: 'Ivysaur', height: 3.3, type: ['grass', 'poison']},
     {name: 'Venusaur', height: 6.7, type: ['grass', 'poison']}
 ];
 
+function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
 //print pokemonList array items to document
 //add emphasis to extra tall pokemon
-pokemonList.forEach( function getPokemonList(item){
+pokemonRepository.getAll().forEach( function getPokemonList(item){
     if ( item.height >= 4) {
         document.write('<p>' + item.name + ', height: ' + item.height + ' - whoa there!' + '</p>');
     } else { 
